504da510d8433255d3efadee2f0798a24b3a6b5f
Derick <derick.1787@gmail.com>
2026-06-16 14:34:02 -0500

    feat: implementar calculo base, logica de hora pico, descuento y resumen de viaje

--- SimuladorTarifa.cs
+++ SimuladorTarifa.cs
@@ -1,19 +1,26 @@
+using System.ComponentModel.DataAnnotations;
+
 public class SimuladorTarifa
 {
     public static void Main(string[] args)
     {
+        string nombre, nombreVehiculo;
+        double distancia, tarifaBase = 0.0, costoKm = 0.0, subtotal = 0.0, descuento = 0.0, total = 0.0;
+        int horas, tipoVehiculo;
+        bool esHoraPico;
+
         Console.WriteLine("==============================================");
         Console.WriteLine("         InDrive - Simulador de Tarifa        ");
         Console.WriteLine("==============================================");
 
         Console.WriteLine("Nombre del Pasajero: ");
-        string nombre = Console.ReadLine();
+        nombre = Console.ReadLine();
 
         Console.WriteLine("Distancia del viaje (en KM): ");
-        double distancia = double.Parse(Console.ReadLine());
+        distancia = double.Parse(Console.ReadLine());
         
         Console.WriteLine("Hora de Salida (0 - 23 hrs): ");
-        int horas = int.Parse(Console.ReadLine());
+        horas = int.Parse(Console.ReadLine());
 
         Console.WriteLine("\nTipo de Vehiculo: ");
         Console.WriteLine("1. Economico ");
@@ -21,8 +28,69 @@ public class SimuladorTarifa
         Console.WriteLine("3. Premium: ");
         Console.WriteLine("4. Moto: ");
         Console.WriteLine("Ingrese una opcion: ");
-        int tipo = int.Parse(Console.ReadLine());
+        tipoVehiculo = int.Parse(Console.ReadLine());
         
+
+        switch(tipoVehiculo)
+        {
+            case 1:
+                nombreVehiculo = "Economico";
+                tarifaBase = 2.0;
+                costoKm = 1.5;
+
+                break;
+            case 2:
+                nombreVehiculo = "Confort";
+                tarifaBase = 3.0;
+                costoKm = 2.0;
+
+                break;
+            case 3:
+                nombreVehiculo = "Premium";
+                tarifaBase = 5.0;
+                costoKm = 3.0;
+
+                break;
+            case 4:
+                nombreVehiculo = "Moto";
+                tarifaBase = 1.5;
+                costoKm = 1.0;
+
+                break;
+            default:
+                Console.WriteLine("Opcion no valida. Fin del programa");
+                return;
+        }
         
+        subtotal = tarifaBase + (costoKm * distancia);
+
+        if ((horas >= 7 && horas <= 9) || (horas >=17 && horas <= 20))
+        {
+            esHoraPico = true;
+            subtotal = subtotal * 1.30;
+        }
+
+        if (distancia > 15)
+        {
+            descuento = subtotal * 0.05;
+            subtotal = subtotal - descuento;
+        }
+
+        total = Math.Round(Math.Max(subtotal, 5.00), 2);
+
+        Console.WriteLine("==============================================");
+        Console.WriteLine("              RESUMEN DEL VIAJE               ");
+        Console.WriteLine("==============================================");
+        Console.WriteLine("Pasajero: " + nombre);
+        Console.WriteLine("Vehiculo: " + nombreVehiculo);
+        Console.WriteLine("Distancia: " + distancia + " km.");
+        Console.WriteLine("Hora Pico: " + (esHoraPico?"Si (+30%)":"No")); //inline if --> condicional? es verdad : es falso;
+        if (descuento > 0)
+        {
+            Console.WriteLine("Descuento: S/." + Math.Round(descuento,2));    
+        }
+        Console.WriteLine("==============================================");
+        Console.WriteLine("La tarifa final es: S/." + total);
+        Console.WriteLine("==============================================");
     }
 }
